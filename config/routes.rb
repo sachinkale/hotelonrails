01ebb1216b5420569d274216b2db8c21c24c5bf2
sachin <brightspark@gmail.com>
2011-01-20 17:14:32 +0530

    adding admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Hsahara::Application.routes.draw do
 
+  devise_for :admins
+
   devise_for :users
 
   delete "home/delete_service" 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110112092605) do
+ActiveRecord::Schema.define(:version => 20110120112645) do
+
+  create_table "admins", :force => true do |t|
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
+  add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
 
   create_table "checkins", :force => true do |t|
     t.datetime "fromdate"
