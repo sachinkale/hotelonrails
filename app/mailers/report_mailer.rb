25d0b5570e5addac8444e4a963c2197ad0294ce6
sachin <brightspark@gmail.com>
2011-05-01 17:21:13 +0530

    removing logging

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -2,9 +2,7 @@ class ReportMailer < ActionMailer::Base
   default :from => "brightspark@gmail.com"
 
   def daily_report(checkins)
-    logger.info('here')
     @checkins = checkins
-    logger.info(@checkins)
     mail(:to => "sachin@hotelsahara.in",
      :subject => "Daily Report")
   end
