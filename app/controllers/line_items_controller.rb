9cf95d846b9edecc835157a69eac044b64b72869
sachin <brightspark@gmail.com>
2011-05-12 20:48:27 +0530

    edit room checkin code

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -11,6 +11,20 @@ class LineItemsController < ApplicationController
   end
 
   def edit_line_item
+    @line_item = LineItem.find(params[:id])
+
+    params[:line_item]["tax"] = 0 if params[:line_item]["tax"] == "" 
+    params[:line_item]["rate"] = 0 if params[:line_item]["rate"] == ""
+    params[:line_item]["extraperson"] = 0 if params[:line_item]["extraperson"] == ""
+    Time.zone = APP_CONFIG['hotel_time_zone']
+    fromdate = Time.local(params[:line_item1][:'fromdate(1i)'].to_i,params[:line_item1][:'fromdate(2i)'].to_i,params[:line_item1][:'fromdate(3i)'].to_i,params[:line_item1][:'fromdate(4i)'].to_i,params[:line_item1][:'fromdate(5i)'].to_i)
+    if fromdate.future?
+      @error = "Cannot Set date / time greater than today"
+    else
+      params[:line_item][:fromdate] = fromdate
+      @line_item.update_attributes(params[:line_item])
+    end
+    @checkin = @line_item.checkin
     
   end
 
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,15 +3,22 @@ class LineItem < ActiveRecord::Base
   belongs_to :checkin
   before_save :block_room
   before_destroy :unblock_room
+
+  validates_numericality_of :rate
+  validates_numericality_of :extraperson
   
   def amount
-    (rate  + extraperson + ((rate + extraperson) * tax/100).to_i ) * no_of_days
+    amount_per_day * no_of_days
   end
+  
+  def amount_per_day
+    (rate  + extraperson + ((rate + extraperson) * tax/100).to_i ) 
+  end
+
 
   def no_of_days
     n = 0
 
-    from = fromdate.in_time_zone(APP_CONFIG['hotel_time_zone'])
     today = Time.now.in_time_zone(APP_CONFIG['hotel_time_zone'])
 
     n = n + 1 if from.hour < APP_CONFIG['hotel_checkout_hour'] 
@@ -21,6 +28,13 @@ class LineItem < ActiveRecord::Base
     return n + (today.to_date - from.to_date).to_i
   end
 
+  def from
+    fromdate.in_time_zone(APP_CONFIG['hotel_time_zone'])
+  end
+
+  def discount
+    (100 - (rate.to_f/room.room_type.baserate.to_f) * 100).round(2)
+  end
 
   private
   def block_room
