2d4929444da71a0a5337afbf59ee96d9ce2281d3
sachin <brightspark@gmail.com>
2011-06-22 20:05:07 +0530

    shift date edit facility

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -13,18 +13,32 @@ class LineItemsController < ApplicationController
   def edit_line_item
     @line_item = LineItem.find(params[:id])
 
+    @checkin = @line_item.checkin
+
     params[:line_item]["tax"] = 0 if params[:line_item]["tax"] == "" 
     params[:line_item]["rate"] = 0 if params[:line_item]["rate"] == ""
     params[:line_item]["extraperson"] = 0 if params[:line_item]["extraperson"] == ""
     Time.zone = APP_CONFIG['hotel_time_zone']
-    fromdate = Time.local(params[:line_item1][:'fromdate(1i)'].to_i,params[:line_item1][:'fromdate(2i)'].to_i,params[:line_item1][:'fromdate(3i)'].to_i,params[:line_item1][:'fromdate(4i)'].to_i,params[:line_item1][:'fromdate(5i)'].to_i)
+
+    fromdate = Time.local(params[:line_item1]["fromdate(1i)"].to_i,params[:line_item1]["fromdate(2i)"].to_i,params[:line_item1]["fromdate(3i)"].to_i,params[:line_item1]["fromdate(4i)"].to_i,params[:line_item1]["fromdate(5i)"].to_i)
+
+
+
     if fromdate.future?
       @error = "Cannot Set date / time greater than today"
     else
       params[:line_item][:fromdate] = fromdate
-      @line_item.update_attributes(params[:line_item])
+      if @line_item.freez
+        todate = Time.local(params[:line_item1]["todate(1i)"].to_i,params[:line_item1]["todate(2i)"].to_i,params[:line_item1]["todate(3i)"].to_i,params[:line_item1]["todate(4i)"].to_i,params[:line_item1]["todate(5i)"].to_i) 
+        if fromdate > todate
+          @error = "Cannot set shift date greater than checkin date"
+        else
+          @checkin.line_items.where(:fromdate => @line_item.todate)[0].update_attribute(:fromdate, todate)
+          params[:line_item][:todate] = todate
+        end
+      end
     end
-    @checkin = @line_item.checkin
+    @line_item.update_attributes(params[:line_item]) if @error.nil? or @error == ""
     @checkin.update_fromdate
   end
 
